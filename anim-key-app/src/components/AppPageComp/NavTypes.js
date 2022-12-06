4bf6cc2bc3ebebad8069a1e28cc95002ca451133
LeonardoVicol <vicolleonardo@gmail.com>
2022-12-06 17:54:05 +0200

    feat update

---
+++ anim-key-app/src/components/AppPageComp/NavTypes.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NavTypes = () => {
+    return(
+        <>
+          <div className="
+            
+          ">
+
+          </div>
+        </>
+    );
+}
+
+export default NavTypes;
